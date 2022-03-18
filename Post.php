c9abe2f4b2a9634d5776a024826869927829f17e
spidertch <99767809+spidertch@users.noreply.github.com>
2022-03-18 18:19:55 -0400

    Add files via upload

---
+++ Post.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+    <title>Spidertch</title>
+</head>
+<body>
+
+<?php include 'index.php' ?>
+
+<section class="home">
+        <div class="text">Posts</div>
+</section>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=divice-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'>
+
+    <title>Spidertch</title>
+</head>
+    <nav class="sidebar close">
+        <header>
+            <div class="image-text">
+                <span class="image">
+                    <img src="icono.png" alt="icono">
+                </span>
+                <div class="text header-text">
+                    <span class="name">
+                        Spidertch    
+                    </span>
+                    <span class="profession">
+                        Pentester, CTF player
+                    </span>
+                </div>
+            </div>
+
+            <i class='bx bxs-chevron-right toggle'></i>
+        </header>
+
+        <div class="menu-bar">
+            <div class="menu">
+                <li class="search-box">
+                        <i class='bx bx-search-alt-2 icon' ></i>
+                        <input type="search" placeholder="Search...">
+                </li>
+                <ul class="menu-links">
+                    <li class="nav-link">
+                        <a href="index.php">
+                            <i class='bx bx-home-alt icon' ></i>
+                            <span class="text nav-text">Inicio</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="Post.php">
+                            <i class='bx bx-book icon'></i>
+                            <span class="text nav-text">Post</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="sobremi.php">
+                            <i class='bx bx-user icon'></i>
+                            <span class="text nav-text">Sobre Mi</span>
+                        </a>
+                    </li>
+                    <li class="nav-link">
+                        <a href="writeup.php">
+                            <i class='bx bx-joystick icon'></i>
+                            <span class="text nav-text">Write Up</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+
+            <div class="bottom-content">
+                <li class="mode">
+                    <div class="moon-sun">
+                        <i class='bx bx-moon icon moon' ></i>
+                        <i class='bx bx-sun icon sun' ></i>
+                    </div>
+                    <span class="mode-text text">Dark Mode</span>
+
+                    <div class="toggle-switch">
+                        <span class="switch">
+
+                        </span>
+                    </div>
+
+                </li>
+            </div>
+        </div>
+    </nav>
+
+    
+    
+
+    <script src="script.js"></script>
+
+</html>
